c700572d07d4f5eb37ae79ae8d3f820a92631411
xxaksed <aksed.336@mail.ru>
2023-02-01 22:26:47 +0300

    header

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import logoImg from "./7222.png"
+import profImg from "./Profile.svg"
+import heartImg from "./Heart.svg"
+import cartImg from "./Cart.svg"
+
+export {profImg,logoImg, heartImg, cartImg}
---
+++ src/componets/Header/style.js
@@ -0,0 +1,61 @@
+import styled from "styled-components"
+
+export const HeaderStyled = styled.div`
+margin: 0px;
+width: 100%;
+height: 70px;
+background: #2d2d2d;
+display: flex;
+align-items: center;
+padding: 0 32px 0 32px;
+text-align: center;
+justify-content: center;
+`
+export const LogoStyled = styled.img`
+width: 70px;
+height: 70px;
+`
+export const WomenStyled = styled.div`
+color: white;
+width: 115px;
+height: 70px;
+display: flex;
+align-items: center;
+font-size: 20px;
+justify-content: space-around;
+&:hover{
+    background-color: #525050
+}
+`
+export const MenStyled = styled.div`
+color: white;
+width: 115px;
+height: 70px;
+display: flex;
+align-items: center;
+font-size: 20px;
+justify-content: space-around;
+&:hover{
+    background-color: #525050
+}
+`
+export const InputStyled = styled.input`
+border-radius: 25px;
+width: 750px;
+height: 38px;
+`
+export const ProfImgStyled = styled.img`
+height: 25px;
+width: 25px;
+margin-left: 10px;
+`
+export const HeartImgStyled = styled.img`
+height: 25px;
+width: 25px;
+margin-left: 10px;
+`
+export const CartImgStyled = styled.img`
+height: 25px;
+width: 25px;
+margin-left: 10px;
+`
